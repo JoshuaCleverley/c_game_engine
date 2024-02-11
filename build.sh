9a845d51942cad827e42a846d8e03c7ae10b5853
Joshua Cleverley <joshuacleverley@protonmail.com>
2024-02-11 18:10:09 +0000

    Removed freetype dependency

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ time="src/engine/time/time.c"
 deps="deps/src/glad.c"
 main="src/main.c src/engine/global.c"
 
-libs="-lGL -lSDL2-2.0 -lSDL2_mixer -lfreetype -Ideps/include/linmath"
+libs="-lGL -lSDL2-2.0 -lSDL2_mixer -Ideps/include/linmath"
 
 gcc -o game $main $deps $io $render $config $time -Ideps/include -Llib $libs
